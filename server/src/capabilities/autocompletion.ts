f82d6b7d13955913d9f54937874bd5ded829b9e9
Radu Dita <hades2510@yahoo.com>
2023-05-26 09:45:05 +0300

    update autocompletion to work with ::

--- server/src/capabilities/autocompletion.ts
+++ server/src/capabilities/autocompletion.ts
@@ -12,6 +12,7 @@ import { findChildrenOf } from "../FindProto";
 import { type LibraryManager } from "../libraries";
 import { type ProtoCompiler } from "../compiler/Compiler";
 import { type TextDocument } from "vscode-languageserver-textdocument";
+import { Token } from "../compiler/Token";
 
 export const addAutoCompletion = (
   connection: Connection,
@@ -40,29 +41,19 @@ export const addAutoCompletion = (
       [];
 
     //	maybe the identifier is from a lib
-    if (options.length === 0) {
-      let lib = null;
-      let currentSize = 1;
-      const parts = partialIdentifier.split(".");
+    if (
+      options.length === 0 &&
+      partialIdentifier.includes(Token.DOUBLE_COLON.toString())
+    ) {
+      const parts = partialIdentifier.split(Token.DOUBLE_COLON.toString());
+      const libName = parts[0];
+      const lib = libManager.getLib(libName);
 
-      //  libraries can contain dots in their names
-      do {
-        const libName = parts.slice(0, currentSize);
+      if (lib) {
+        const identifierWithoutLib = parts[1];
 
-        lib = libManager.getLib(libName.join("."));
-
-        if (lib != null) {
-          //	get compilers for files that have this lib
-          const identifierWithoutLib = parts.slice(currentSize).join(".");
-
-          options = findChildrenOf(identifierWithoutLib, lib.rootProto);
-          if (options.length > 0) {
-            break;
-          }
-        }
-
-        currentSize++;
-      } while (currentSize <= parts.length);
+        options = findChildrenOf(identifierWithoutLib, lib.rootProto);
+      }
     }
 
     return options.map((op) => ({
